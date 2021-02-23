ff7fbced07982e38fbf90a19041f35c42c245bed
Bryan Pan <bryanpan342@gmail.com>
2021-02-23 00:12:06 -0800

    feat: simple logo

--- webpack.common.js
+++ webpack.common.js
@@ -32,6 +32,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: './index.html',
+      favicon: './src/assets/favicon.ico'
     }),
     new CleanWebpackPlugin(),
   ],
