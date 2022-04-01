f079a405ec4a3892ad1e5b68d32bd05e1d73484d
Bryan Pan <bryanpan342@gmail.com>
2022-03-31 23:10:23 -0700

    feat: splash complete on desktop

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 import Layout from '../components/Layout';
 import styles from '../styles/Home.module.scss';
@@ -6,8 +7,19 @@ export default function Home(): JSX.Element {
   return (
     <Layout id={styles.container}>
       <div id={styles.splash}>
-        <h3>BRYAN PAN</h3>
-        <h1>Building cool shit with magic.</h1>
+        <div id={styles.designer} className={styles['background-text']}>
+          DESIGNER
+        </div>
+        <div id={styles.developer} className={styles['background-text']}>
+          DEVELOPER
+        </div>
+        <div id={styles['splash-text']}>
+          <h3>BRYAN PAN</h3>
+          <h1>Building cool shit with magic.</h1>
+        </div>
+        <div id={styles.magic}>
+          <Image src="/magic.png" layout="responsive" width="293.96px" height="230.81px" />
+        </div>
       </div>
       <div id={styles['work-container']}>
         <h3>WORK</h3>
